eac665259646680fcb97e8972917e89cdfed4bde
CharakaGunathilake <gunathilakec122@gmail.com>
2024-11-03 15:11:46 +0530

    new

--- src/app/pages/dashboard/meals/meals.component.ts
+++ src/app/pages/dashboard/meals/meals.component.ts
@@ -32,7 +32,7 @@ export class MealsComponent implements OnInit {
           'rgb(75, 192, 192)',
           'rgb(255, 205, 86)',
           'rgb(201, 203, 207)',
-          'rgb(54, 162, 235)'
+          'rgb(54, 162, 235)',
         ]
       }]
     };
