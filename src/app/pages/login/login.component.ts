fd7797467443a70379b7d1b51ba80321ff24a489
CharakaGunathilake <gunathilakec122@gmail.com>
2024-11-07 17:39:44 +0530

    new commit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RegisterComponent } from '../register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, RegisterComponent],
+  imports: [RegisterComponent, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  
+export class LoginComponent implements OnInit {
+  private baseUrl: String = "http://localhost:8080/";
+  protected rememberMe: boolean = false;
+  private loginObj: any;
+
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    const currentUser = localStorage.getItem("currentUser");
+    console.log(currentUser);
+  }
+  protected login = {
+    userName: "",
+    password: ""
+  }
+  protected async checkLogin() {
+    if (await this.validLogin(this.login)) {
+      localStorage.setItem("rememberedLogin", this.rememberMe === true ? JSON.stringify(true) : JSON.stringify(false))
+      this.router.navigate(["/dashboard/home"])
+    } else {
+      console.log("invalid");
+
+    }
+  }
+  private async validLogin(login: any): Promise<boolean> {
+    let response = await fetch(this.baseUrl + "login/get-login-byId/" + `${login.userName}`);
+    let body = await response.json();
+    this.loginObj = body;
+    return this.loginObj.username === login.userName && this.loginObj.password === login.password;
+  }
 }
 
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,19 +1,92 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from "@angular/forms";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink,LoginComponent,FormsModule],
+  imports: [RouterLink, FormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent implements OnInit{
+
+export class RegisterComponent implements OnInit {
+  private baseUrl: String = "http://localhost:8080/";
+  private header = { "Content-Type": "application/json" };
   ngOnInit(): void {
   }
-   public firstName: string ="";
-    
+  protected passwordNew: string = "";
+  protected passwordConfirm: string = "";
+  protected expression: boolean = false;
+  protected userObj = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    regDate: new Date()
+  }
+  protected async showData(): Promise<void> {
+    if (this.matchingPassword()) {
+      if (await this.checkUserName(this.userObj.username)) {
+        console.log("Username already taken");
+      } else {
+        console.log(this.userObj);
+        this.saveUser();
+        localStorage.setItem("currentUser", `${this.userObj.username}`);
+        this.saveLogin();
+      }
+    } else {
+      console.log("Password fields doesn't match")
+    }
+  }
+  private async checkUserName(username: String): Promise<boolean> {
+    let response = await fetch(this.baseUrl + "login/check-username/" + `${username}`);
+    return await response.json();
+  }
 
+  private matchingPassword(): boolean {
+    if (this.passwordNew === this.passwordConfirm) {
+      this.userObj.password = this.passwordConfirm;
+      return true;
+    } return false;
+  }
+  private user: any = {
+    "firstName": this.userObj.firstName,
+    "lastName": this.userObj.lastName,
+    "gender": "Male",
+    "birthDay": "2000-05-15",
+    "age": 24,
+    "height": 5.8,
+    "weight": 70,
+    "email": this.userObj.email,
+    "status": "true",
+    "regDate": this.userObj.regDate
+  }
+
+  private login: any = {
+    "username": this.userObj.username,
+    "password": this.userObj.password
+  }
+
+  private async saveUser() {
+    let response = await fetch(this.baseUrl + "user/add-user", {
+      method: "POST",
+      headers: this.header,
+      body: JSON.stringify(this.user)
+    })
+    let body = await response.json()
+    alert(JSON.stringify(body));
+  }
+
+  private async saveLogin() {
+    let response = await fetch(this.baseUrl + "login/add-login", {
+      method: "POST",
+      headers: this.header,
+      body: JSON.stringify(this.login)
+    })
+    let body = await response.json()
+    alert(JSON.stringify(body));
+  }
 }
